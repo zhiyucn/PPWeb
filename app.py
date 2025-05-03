684fbb7fe92aa9cff44d871aecf99c6b4deaedbb
zhiyucn <zhiyuxl@outlook.com>
2025-05-03 12:13:07 +0800

    refactor: 更新文件扩展名及相关逻辑
    将文件扩展名从`.pp`更改为`.ppw`，并更新了`app.py`中的文件处理逻辑以匹配新的扩展名。同时更新了`README.md`中的说明以反映这一变化。

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 async def index(path="/"):
     # 如果是/，返回index.pp
     if path == "/":
-        return str(await main(open(os.path.join(os.path.dirname(__file__),"files", "index.pp"), "r").read()))
+        return str(await main(open(os.path.join(os.path.dirname(__file__),"files", "index.ppw"), "r").read()))
     else:
         file_path = os.path.join(os.path.dirname(__file__), "files", path)
         print(file_path)
-        if not os.path.exists(file_path) or not path.endswith('.pp'):
+        if not os.path.exists(file_path):
             return "404 Not Found", 404
         return str(await main(open(file_path, "r").read()))
 
